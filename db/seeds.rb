20f1805e4e2b90ad8dcdd38ab820232edbb0c378
deepika joshi <upreti.deepika@gmail.com>
2023-03-11 23:26:21 +0800

    added seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+User.destroy_all
+
+@user_count = User.count
+if @user_count.zero?
+  puts "Creating users..."
+  4.times do
+    User.create!(
+      email: Faker::Internet.email,
+      password: Faker::Internet.password
+    )
+  end
+  puts "Finished!"
+end
